02b1e1b9b709fc442525538f731e0df9ebb70753
Jonas Camargo <jonascamargo999@gmail.com>
2025-05-27 08:22:09 -0300

    feat: adiciona controller para customer

---
+++ src/main/java/com/safebank/demo/controllers/CustomerController.java
@@ -0,0 +1,62 @@
+package com.safebank.demo.controllers;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.safebank.demo.domains.Customer;
+import com.safebank.demo.dtos.CustomerDTO;
+import com.safebank.demo.services.AccountService;
+import com.safebank.demo.services.CustomerService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/clientes")
+public class CustomerController {
+    private final CustomerService customerService;
+    private final AccountService accountService;
+
+
+    public  CustomerController(CustomerService customerService, AccountService accountService) {
+        this.customerService = customerService;
+        this.accountService = accountService;
+    }
+
+    
+    @GetMapping("/")
+    public ResponseEntity<?> getCustomers() {
+        List<Customer> customers = customerService.getCustomers();
+        return ResponseEntity.status(HttpStatus.OK).body(customers);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getCustomerById(@PathVariable(value = "id") Long id) {
+        Customer customer = customerService.getCustomerById(id);
+        return ResponseEntity.status(HttpStatus.OK).body(customer);
+
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<Customer> createCustomer(@RequestBody @Valid CustomerDTO customerDTO) {
+        Customer createdCustomer = customerService.createCustomer(customerDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdCustomer);
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteCustomer(@PathVariable(value = "id") Long id) {
+        this.customerService.deleteCustomer(id);
+        return ResponseEntity.status(HttpStatus.OK).body("Customer deleted successfully.");
+    }
+
+
+}
