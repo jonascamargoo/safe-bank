b57d6c8ec31c6e82fc5d60075958cbcfa1cb67cf
Jonas Camargo <jonascamargo999@gmail.com>
2025-06-03 19:09:29 -0300

    feat: add RestErrorMessage and RestExceptionHandler classes for improved error handling

---
+++ src/main/java/com/safebank/demo/exceptions/RestErrorMessage.java
@@ -0,0 +1,20 @@
+package com.safebank.demo.exceptions;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RestErrorMessage {
+    private HttpStatus status;
+    private String message;
+
+    public RestErrorMessage(HttpStatus status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+
+}
