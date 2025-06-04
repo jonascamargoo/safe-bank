47a757d76efbfe742b83bbdb423ab10cccab175e
Jonas Camargo <jonascamargo999@gmail.com>
2025-06-03 21:17:50 -0300

    inicializa angular na versão 19

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -4,9 +4,9 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
 })
-export class App {
-  protected title = 'safe-bank-frontend';
+export class AppComponent {
+  title = 'ui';
 }
